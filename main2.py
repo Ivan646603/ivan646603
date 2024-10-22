c5dffb09891e149ad11df8a3cce942f00cf4da12
Ivan646603 <nesterov850603@gmail.com>
2024-10-23 00:57:58 +1200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,5 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = (2 and 1)
+print(number_of_hours_spent / number_of_completed_homework_assignments)
